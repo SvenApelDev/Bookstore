d204fc687a0b6e1801518ce09dca6a1e987bcbd1
Sven Apel <svap1201@gmail.com>
2026-07-17 10:25:00 +0200

    Add books JSON array with title, image, price, likes and comments

---
+++ script.js
@@ -0,0 +1,113 @@
+
+
+let books = [
+    {
+        'name': 'Erstes Buch',
+        'author': 'Sven Apel',
+        'likes': 1256,
+        'liked': true,
+        'price': 12.80,
+        'publishedYear': 1980,
+        'genre': 'Fantasy',
+        'comments': [
+            {
+                'name': 'Leser123',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'FantasyGuru',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'BookFan',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'Unbekannt',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            }
+
+        ]
+    },
+    {
+        'name': 'Zweites Buch',
+        'author': 'Emil Eckl',
+        'likes': 126,
+        'liked': true,
+        'price': 15.60,
+        'publishedYear': 2000,
+        'genre': 'Drama',
+        'comments': [
+            {
+                'name': 'Leser123',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'FantasyGuru',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'BookFan',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'Unbekannt',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            }
+        ]
+    },
+    {
+        'name': 'Drittes Buch',
+        'author': 'Nick Tunger',
+        'likes': 1263,
+        'liked': true,
+        'price': 19.10,
+        'publishedYear': 2001,
+        'genre': 'Adventure',
+        'comments': [
+            {
+                'name': 'Leser123',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'FantasyGuru',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'BookFan',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'Unbekannt',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            }
+        ]
+    },
+    {
+        'name': 'Viertes Buch',
+        'author': 'Linda Tunger',
+        'likes': 304,
+        'liked': true,
+        'price': 10.90,
+        'publishedYear': 2012,
+        'genre': 'Comedy',
+        'comments': [
+            {
+                'name': 'Leser123',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'FantasyGuru',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'BookFan',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            },
+            {
+                'name': 'Unbekannt',
+                'comment': 'Die Handlung ist sehr gut gelungen und handelt von einem abenteuerlichen Leben.'
+            }
+        ]
+    }
+];
